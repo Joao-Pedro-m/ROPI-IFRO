898b0d10c21f97fba62ee49de1e8a3dbefa4837e
kyuubi V <viniciusclash2303@gmail>
2023-05-16 14:26:39 -0400

    pasta templates criada

--- exemplo/models.py
+++ exemplo/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class ProjetosIntegradores(models.Model):
+    id = models.AutoField()
+    titulo = models.CharField(max_length=45)
+    periodo_inicial = models.DateField()
+    periodo_final = models.DateField()
+    resumo = models.CharField(max_length=250)
+    resultados = models.CharField(max_length=150)
+    imagem = models.BinaryField()
+    Curso_idCurso = models.IntegerField()
+    Ano_idAno = models.IntegerField()
+    
+    
+class Cursos(models.Model):
+    id = models.AutoField()
+    tipo_curso = models.CharField(max_length=45)
+    
+class Ano(models.Model):
+    id = models.AutoField()
+    ano = models.IntegerField()
+    
+    
+class Administradores(models.Model):
+    id = models.AutoField()
+    nome = models.CharField(max_length=45)
+    matricula_API = models.BigIntegerField()
+    senha_API = models.CharField(max_length=45)
+    
+    def __str__(self):
+        return self.nome
+    
+    
